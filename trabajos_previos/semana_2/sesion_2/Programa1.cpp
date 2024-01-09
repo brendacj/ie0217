764e309cefb64c56c9a07286793e6f61cafead6f
brendacj <brenda.castrojimenez@ucr.ac.cr>
2024-01-09 16:49:14 -0600

    Ajustes a la organización de las sesiones
    Signed-off-by: brendacj <brenda.castrojimenez@ucr.ac.cr>

--- trabajos_previos/semana_2/sesion_2/Programa1.cpp
+++
@@ -1,8 +0,0 @@
-# include <iostream>
-int count ;
-extern void write_extern(); // Relacioanda con otro porgrama (Programa2)
-
-main() {
-    count = 5;
-    write_extern();
-}
--- trabajos_previos/semana_2/sesion_2/Programa2.cpp
+++
@@ -1,6 +0,0 @@
-# include <iostream>
-extern int count ; // Relacionada con otro programa (Programa1)
-extern void write_extern(void){
-    std::cout << "Count is " << count << std::endl;
-}
-
