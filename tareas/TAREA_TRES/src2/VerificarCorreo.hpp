f80f864bfd3be8d43c7ec948cebc04840dbebc55
brendacj <brenda.castrojimenez@ucr.ac.cr>
2024-01-23 06:53:15 -0600

    Agrego el el archivo .hpp de la clase Verificar Correo
    Signed-off-by: brendacj <brenda.castrojimenez@ucr.ac.cr>

---
+++ tareas/TAREA_TRES/src2/VerificarCorreo.hpp
@@ -0,0 +1,13 @@
+#ifndef CORREO_HPP
+#define CORREO_HPP
+
+#include <regex>
+#include <iostream>
+
+class VerificarCorreo {
+    public:
+        bool verificarCorreo(std::string &correo);
+
+};
+
+#endif
