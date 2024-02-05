8c7fb60e6a5c8d5a55977bfa6603c010d340256f
brendacj <brenda.castrojimenez@ucr.ac.cr>
2024-02-05 03:45:03 -0600

    Implemento la obtención de datos como una clase
    Signed-off-by: brendacj <brenda.castrojimenez@ucr.ac.cr>

--- tareas/TAREA_CINCO/src1/ObtencionDatos.py
+++ tareas/TAREA_CINCO/src1/ObtencionDatos.py
@@ -1,18 +1,26 @@
 import pandas as pd
-try:
-    # Leer un archivo separado por comas, se guarda en un dataFrame
-    aerolineas = pd.read_csv('aerolineas.csv')
-    # Eliminar las filas con valores nulos
-    aerolineas = aerolineas.dropna()
-    # Asegurarse de que los tipos de las columnas relacionadas con los id o meses sean enteros
-    aerolineas[['ORIGIN_AIRPORT_ID', 'DEST_AIRPORT_ID', 'MONTH']] = aerolineas[['ORIGIN_AIRPORT_ID', 'DEST_AIRPORT_ID', 'MONTH']].astype(int)
-    # Asegurarse de que los tipos de la columnas de distancias sean floats
-    aerolineas[['PASSENGERS', 'DISTANCE']] = aerolineas[['PASSENGERS', 'DISTANCE']].astype(float)
-except FileNotFoundError:
-    print("El archivo no se encontró. Verifique")
-except Exception as e:
-    print(f"Se produjo un error inesperado durante la carga de datos: {e}")
 
 
-# Mostrar los de aerolineas
-print(aerolineas.head())
+class ObtencionDatos():
+    def __init__ (self):
+        self.archivo = 'aerolineas.csv'
+        self.aerolineas = pd.DataFrame()
+    
+    def obtencionLimpieza(self):
+        try:
+            # Leer un archivo separado por comas, se guarda en un dataFrame
+            self.aerolineas = pd.read_csv(self.archivo)
+            # Eliminar las filas con valores nulos
+            self.aerolineas = self.aerolineas.dropna()
+            # Asegurarse de que los tipos de las columnas relacionadas con los id o meses sean enteros
+            self.aerolineas[['ORIGIN_AIRPORT_ID', 'DEST_AIRPORT_ID', 'MONTH']] = self.aerolineas[['ORIGIN_AIRPORT_ID', 'DEST_AIRPORT_ID', 'MONTH']].astype(int)
+            # Asegurarse de que los tipos de la columnas de distancias sean floats
+            self.aerolineas[['PASSENGERS', 'DISTANCE']] = self.aerolineas[['PASSENGERS', 'DISTANCE']].astype(float)
+        except FileNotFoundError:
+            print("El archivo no se encontró. Verifique")
+        except Exception as e:
+            print(f"Se produjo un error inesperado durante la carga de datos: {e}")
+
+
+        # Mostrar los de aerolineas
+        print(self.aerolineas.head())
