622c37abb09d7e0b1aa343378cc396ffb299f4af
brendacj <brenda.castrojimenez@ucr.ac.cr>
2024-02-06 21:13:14 -0600

    Agrego los comentarios al programa de la práctica 2
    Signed-off-by: brendacj <brenda.castrojimenez@ucr.ac.cr>

--- tareas/TAREA_CINCO/src2/parte_practica_2.py
+++ tareas/TAREA_CINCO/src2/parte_practica_2.py
@@ -1,23 +1,15 @@
+"""
+Este software está sujeto a los términos y condiciones de la Licencia MIT.
+Entre otras cosas se establece que si se utiliza o distribuye partes
+sustanciales del programa, se deben incluir el aviso de derechos de autor
+y el aviso de permiso en tu distribución.
+"""
+
 import pandas as pd
 import numpy as np
-## Crear una lista, filas y columnas, como un diccionario
 
 """
-df = pd.DataFrame(
-{
-    "Name": [
-    "Brenda", "Henry",
-    "Isabela", "Christopher",
-    "Genesis", "Kevin",
-    ],
-    "Matemáticas": [10, 9, 8, 6, 8, 8.5],
-    "Español": [9, 8, 10, 7, 8, 9.5],
-    "Ciencias": [9, 8, 9.5, 4, 5.5, 7.5],
-    "Estudios Sociales": [8, 7, 10, 8, 6, 9],
-    "Artes plásticas": [10, 6, 6, 9.5, 6.5, 10],
-    
-}
-)
+Definición de los arreglos de tipo numpy para las operaciones
 """
 calificaciones = np.array(
     [
@@ -31,30 +23,30 @@ calificaciones = np.array(
 )
 
 asignaturas = np.array(
-    ["Matemáticas", "Español", "Ciencias", "Estudios Sociales", "Artes plásticas"]
+    ["Matemáticas", "Español", "Ciencias",
+     "Estudios Sociales", "Artes plásticas"]
 )
 
 nombres = np.array(
     ["Brenda", "Henry",
-    "Isabela", "Christopher",
-    "Genesis"]
+     "Isabela", "Christopher",
+     "Genesis"]
 )
 
 
-"""
 def resumenGeneral():
-    califPorEstudiante = []
-    califPorAsignatura = []
-    for index, filas in df.iterrows():
-        promedioFila = filas.mean()
-        califPorEstudiante.append(promedioFila)
+    """
+    Calcula resumen general de calificaciones.
 
-    for columnas in df.columns:
-        promedioColumna = columnas.mean()
-        califPorAsignatura.append(promedioColumna)
+    Calcula el promedio y la nota máxima por estudiante, la sumatoria
+    de calificaciones por asignatura y el promedio por asignatura.
 
-"""
-def resumenGeneral():
+    Returns:
+    -------
+    tuple: Una tupla que contiene el promedio por estudiante,
+    la nota máxima por estudiante, la sumatoria por
+    asignatura y el promedio por asignatura.
+    """
 
     promPorEstudiante = np.mean(calificaciones, axis=1)
 
@@ -64,46 +56,69 @@ def resumenGeneral():
 
     promPorAsigantura = np.mean(calificaciones, axis=0)
 
-    return(promPorEstudiante, maximoPorEstudiante, sumatoriaPorAsignatura, promPorAsigantura)
+    return (promPorEstudiante, maximoPorEstudiante,
+            sumatoriaPorAsignatura, promPorAsigantura)
 
 
-def imprimir(promPorEstudiante, maximoPorEstudiante, sumatoriaPorAsignatura, promPorAsigantura):
+def imprimir(promPorEstudiante, maximoPorEstudiante,
+             sumatoriaPorAsignatura, promPorAsigantura):
+    """
+    Imprime resumen de calificaciones.
 
-    df= pd.DataFrame(calificaciones, columns=asignaturas)
+    Imprime la tabla de calificaciones, la sumatoria por asignatura,
+    el promedio por asignatura, el promedio por estudiante y la nota
+    máxima por estudiante.
+
+    Parameters:
+    ----------
+    promPorEstudiante : array
+        Promedio por estudiante.
+    maximoPorEstudiante : array
+        Nota máxima por estudiante.
+    sumatoriaPorAsignatura : array
+        Sumatoria por asignatura.
+    promPorAsigantura : array
+        Promedio por asignatura.
+    """
+
+    df = pd.DataFrame(calificaciones, columns=asignaturas)
     df.insert(0, 'Estudiante', nombres)
     print("\n### Tabla de datos ###")
     print(df)
-    """
-    df1= pd.DataFrame([sumatoriaPorAsignatura], columns=asignaturas)
-    print("\nSumatoria por asignatura")
-    print(df1)
-    df2= pd.DataFrame([promPorAsigantura], columns=asignaturas)
-    print("\nPromedio por asignatura")
-    print(df2)
-    """
-    df1= pd.DataFrame(sumatoriaPorAsignatura, columns=["Sumatoria"])
+
+    df1 = pd.DataFrame(sumatoriaPorAsignatura, columns=["Sumatoria"])
     df1.insert(0, 'Asignatura', asignaturas)
     print("\n## Sumatoria por asignatura ##")
     print(df1)
 
-    df2= pd.DataFrame(promPorAsigantura, columns=["Promedio"])
+    df2 = pd.DataFrame(promPorAsigantura, columns=["Promedio"])
     df2.insert(0, 'Asignatura', asignaturas)
     print("\n## Promedio por asignatura ##")
     print(df2)
 
-    df3= pd.DataFrame(promPorEstudiante, columns=["Promedio"])
+    df3 = pd.DataFrame(promPorEstudiante, columns=["Promedio"])
     df3.insert(0, 'Estudiante', nombres)
     print("\n## Promedio por estudiante ##")
     print(df3)
 
-    df4= pd.DataFrame(maximoPorEstudiante, columns=["Nota"])
+    df4 = pd.DataFrame(maximoPorEstudiante, columns=["Nota"])
     df4.insert(0, 'Estudiante', nombres)
     print("\n## Nota máxima por estudiante ##")
     print(df4)
 
-def main():
-    promPorEstudiante, maximoPorEstudiante, sumatoriaPorAsignatura, promPorAsigantura = resumenGeneral()
-    imprimir(promPorEstudiante, maximoPorEstudiante, sumatoriaPorAsignatura, promPorAsigantura)
 
-main()
-#resumenGeneral()
+def main():
+    """
+    Función principal.
+
+    Ejecuta el análisis de calificaciones e imprime el resumen.
+    """
+    promPorEstudiante, maximoPorEstudiante, sumatoriaPorAsignatura, \
+        promPorAsigantura = resumenGeneral()
+    imprimir(promPorEstudiante, maximoPorEstudiante,
+             sumatoriaPorAsignatura, promPorAsigantura)
+
+
+if __name__ == "__main__":
+    # Llamar la main
+    main()
