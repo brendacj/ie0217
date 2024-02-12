a5a12712266a5858e6db90a30a05252bac3b001b
brendacj <brenda.castrojimenez@ucr.ac.cr>
2024-02-12 04:29:55 -0600

    Agrego el método para calcular la eficiencia de las regresiones
    Signed-off-by: brendacj <brenda.castrojimenez@ucr.ac.cr>

--- tareas/TAREA_SEIS/cars_analysis/regression.py
+++ tareas/TAREA_SEIS/cars_analysis/regression.py
@@ -12,9 +12,9 @@ class Regresiones ():
         self.datos = ObtencionDatos()
         self.datos.obtencionLimpieza()
 
-    def regresionLineal(self, X, y):
-        X = self.datos.dataSet[[X]]  
-        y = self.datos.dataSet[y]
+    def regresionLineal(self, XX, yy):
+        X = self.datos.dataSet[[XX]]  
+        y = self.datos.dataSet[yy]
 
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -23,32 +23,50 @@ class Regresiones ():
 
         y_pred = regression_model.predict(X_test)
 
+        self.medirRendimiento(y_test, y_pred)
+
         plt.scatter(X_test, y_test, color='blue')
         plt.plot(X_test, y_pred, color='red', linewidth=2)
-        plt.xlabel(X.replace('_', ' '))
-        plt.ylabel(y.replace('_', ' '))
-        plt.title('Regresión Lineal: '+ X.replace('_', ' ') + ' vs. '+ y.replace('_', ' '))
+        plt.xlabel(XX.replace('_', ' '))
+        plt.ylabel(yy.replace('_', ' '))
+        plt.title('Regresión Lineal: '+ XX.replace('_', ' ') + ' vs. '+ yy.replace('_', ' '))
+        #plt.title('Regresión Lineal: ')
         plt.show()
 
-    def regresionNoLineal(self, X, y, grado = 2):
-        X = self.datos.dataSet[[X]]  
-        y = self.datos.dataSet[y]
-        modelo_polinomico = make_pipeline(PolynomialFeatures(grado), LinearRegression())
-        modelo_polinomico.fit(X, y)
+    def regresionNoLineal(self, XX, yy, grado = 2):
+        X = self.datos.dataSet[[XX]]
+        y = self.datos.dataSet[yy]
 
-        X_test = np.linspace(0, 2, 100).reshape(100, 1)
-        y_pred = modelo_polinomico.predict(X_test)
+        X_train_nonlinear, X_test_nonlinear, y_train_nonlinear, y_test_nonlinear = train_test_split(X, y, test_size=0.2, random_state=42)
+
+        modelo_polinomico = make_pipeline(PolynomialFeatures(grado), LinearRegression())
+        modelo_polinomico.fit(X_train_nonlinear, y_train_nonlinear)
+
+        #X_test = np.linspace(0, 2, 100).reshape(100, 1)
+        y_pred = modelo_polinomico.predict(X_test_nonlinear)
+
+        self.medirRendimiento(y_test_nonlinear, y_pred)
 
         plt.scatter(X, y)
-        plt.plot(X_test, y_pred, color = 'red',
+        plt.plot(X_test_nonlinear, y_pred, color = 'red',
                 label = f'Regresión Polinómica ({grado} grado)')
-        plt.title('Regresión no lineal (Polinomica): '+ X.replace('_', ' ') + ' vs. '+ y.replace('_', ' '))
-        plt.xlabel(X.replace('_', ' '))
-        plt.ylabel(y.replace('_', ' '))
+        plt.title('Regresión no lineal (Polinomica): '+ XX.replace('_', ' ') + ' vs. '+ yy.replace('_', ' '))
+        plt.xlabel(XX.replace('_', ' '))
+        plt.ylabel(yy.replace('_', ' '))
         plt.legend()
         plt.show()
+
+    def medirRendimiento(self, y_test, y_pred):
+        mae = mean_absolute_error(y_test, y_pred)
+        mse = mean_squared_error(y_test, y_pred)
+        r2 = r2_score(y_test, y_pred)
+
+        print("Redimiento de la regresión: ")
+        print (f'MAE: {mae:.2f}')
+        print (f'MSE: {mse:.2f}')
+        print (f'R^2: {r2:.2f}')
 
 regresion = Regresiones ()
 regresion.datos.dataSet = regresion.datos.dataSet.sort_values(by='year')
 print(regresion.datos.dataSet.head())
-regresion.regresionLineal('year','selling_price')
+regresion.regresionNoLineal('year','selling_price')
