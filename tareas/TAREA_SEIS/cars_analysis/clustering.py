b4720a43f16c63cc84b69933c43a19b2a7b4e5a4
brendacj <brenda.castrojimenez@ucr.ac.cr>
2024-02-12 22:25:37 -0600

    Agrego la implementación del método de la silueta
    Signed-off-by: brendacj <brenda.castrojimenez@ucr.ac.cr>

--- tareas/TAREA_SEIS/cars_analysis/clustering.py
+++ tareas/TAREA_SEIS/cars_analysis/clustering.py
@@ -2,6 +2,7 @@ from ObtenerData import ObtencionDatos
 import numpy as np
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
+from sklearn.metrics import silhouette_score
 
 
 class Clustering ():
@@ -12,8 +13,6 @@ class Clustering ():
     def kmeans (self, columna1, columna2):
         X = self.datos.dataSet[[columna1, columna2]].values
 
-        self.elbow (X)
-
         # Especificaciones de gráficos con los puntos sin organizar
         plt.figure(figsize=(12, 5))
         plt.subplot(1, 2, 1)
@@ -21,8 +20,8 @@ class Clustering ():
         plt.scatter(X[:, 0], X[:, 1], c='blue', marker='o')
         # Etiquetas y titulo
         plt.title('Puntos de Datos Aleatorios')
-        plt.xlabel('Caracteristica 1')
-        plt.ylabel('Caracteristica 2')
+        plt.xlabel(columna1.replace('_', ' '))
+        plt.ylabel(columna2.replace('_', ' '))
 
 
         kmeans = KMeans(n_clusters=3)
@@ -55,7 +54,8 @@ class Clustering ():
         # Mostrar loa gráficos
         plt.show()
     
-    def elbow (self, X):
+    def elbow (self, columna1, columna2):
+        X = self.datos.dataSet[[columna1, columna2]].values
         # Calcular la inercia de diferentes valores
         inertias = []
         for k in range(1, 11):
@@ -73,8 +73,29 @@ class Clustering ():
         plt.ylabel('Inercia')
         plt.show()
 
+    def silhouette (self, columna1, columna2):
+
+        X = self.datos.dataSet[[columna1, columna2]].values
+
+       #Lista de siluetas
+        silhouette_scores = []
+        for k in range(2, 20):
+            # Calcular siluetas para 18 elementos con números aletorios
+            kmeans = KMeans(n_clusters=k, random_state=42)
+            kmeans.fit(X)
+            score = silhouette_score(X, kmeans.labels_)
+            silhouette_scores.append(score)
+
+        # Grafico de linea según las siluetas
+        plt.plot(range(2, 20), silhouette_scores, marker='o')
+        plt.title('Método de la Silueta')
+        plt.xlabel('Número de Clusters (k)')
+        plt.ylabel('Coeficiente de Silueta')
+        plt.show() 
+
 
 cluster = Clustering ()
 #cluster.datos.dataSet = cluster.datos.dataSet.sort_values(by='year')
+cluster.silhouette ('year','selling_price')
 cluster.kmeans ('year','selling_price')
 # year,selling_price,km_driven
